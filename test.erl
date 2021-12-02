1e20bc79901bd8648cbd7b70eb4a6b58b5abc814
Christian Martinez <christianmmartinex0823@gmail.com>
2021-12-01 21:45:56 -0700

    first commit

---
+++ test.erl
@@ -0,0 +1,31 @@
+-module(test).
+-export([start/0, client/2, server/0]).
+
+client(N, ClientPID) ->
+  ClientPID ! {N + 1, self()},
+  receive
+    continue ->
+      io:format("Client Continuing... ~n", []),
+      client(N + 1, ClientPID);
+    stop ->
+      io:format("Client Received 'Stop' command.  Stopping.~n", []),
+      ClientPID ! done
+  end.
+
+server() ->
+  receive
+    done ->
+      io:format("Server Received 'done'~n", []);
+    {N, ServerPID} ->
+      case (N < 10) of
+        false -> ServerPID ! stop;
+        true -> ServerPID ! continue
+      end,
+      io:format("Server got ~p ~n", [N]),
+      server()
+  end.
+
+start() ->
+  ServerPID = spawn(test, server, []),
+  spawn(test, client, [0, ServerPID]).
+
